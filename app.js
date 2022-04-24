1d1cee6697c1c754a4832900f611c1e287d528dc
TuyenNguyen1302 <tuyenbb1302@gmail.com>
2022-04-24 23:38:20 +0700

    update design and responsive

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-document.querySelector('.toggle').addEventListener('click', function () {
-    document.querySelector('.menu').classList.toggle('active');
+document.querySelector('.toggle i').addEventListener('click', function () {
+    document.querySelector('.menu').classList.add('active');
     document.querySelector('.menu').addEventListener('click', function () {
         document.querySelector('.menu').classList.remove('active');
     })
 });
 
-function togglePopup(e) {
-    document.querySelector('.popup').classList.toggle('hide');
-    document.querySelector('body').classList.toggle('scroll-none');
+document.querySelectorAll('.open-popup').forEach(project => {
+    project.addEventListener('click', () => {
+        let pro = project.getAttribute('data-project');
+        document.querySelectorAll('.popup').forEach(popup => {
+            let pop = popup.getAttribute('data-popup');
+            if (pro == pop) {
+                popup.classList.remove('hide');
+                document.querySelector('body').classList.add('scroll-none');
+            }
+
+            document.querySelectorAll('.btn-close').forEach(close => {
+                close.addEventListener('click', () => {
+                    popup.classList.add('hide');
+                    document.querySelector('body').classList.remove('scroll-none');
+                })
+            })
+        })
+    })
+})
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    console.log(document.documentElement.scrollTop);
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.toggle').style.cssText = `background: #efcac3;`;
+    } else {
+        document.querySelector('.toggle').style.cssText = `background: transperant;`;
+    }
 }
-document.querySelectorAll('.open-popup').forEach(e => {
-    e.addEventListener('click', togglePopup);
+
+document.querySelectorAll('.header a').forEach(e => {
+    e.addEventListener('click', function () {
+        document.querySelectorAll('.header a').forEach(e => {
+            e.classList.remove('show');
+        });
+        this.classList.add('show');
+    })
 });
-document.querySelector('.btn-close').addEventListener('click', togglePopup);
